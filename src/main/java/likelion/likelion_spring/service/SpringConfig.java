f1bbb858da47f6d033ca12923d559f8d047c4b6c
hee0543 <chaehee0543@gmail.com>
2026-04-04 01:40:25 +0900

    [섹션7] 21. 순수 JDBC

--- src/main/java/likelion/likelion_spring/service/SpringConfig.java
+++ src/main/java/likelion/likelion_spring/service/SpringConfig.java
@@ -1,14 +1,26 @@
 package likelion.likelion_spring.service;
 
+import likelion.likelion_spring.repository.JdbcMemberRepository;
 import likelion.likelion_spring.repository.MemberRepository;
 import likelion.likelion_spring.repository.MemoryMemberRepository;
 import likelion.likelion_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.swing.*;
 
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource=dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +29,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
