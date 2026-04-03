b35774eaac100e1e38ff185d1619374cb97fbfbd
hee0543 <chaehee0543@gmail.com>
2026-04-03 23:22:46 +0900

    temp: PR 생성을 위한 커밋

---
+++ src/main/java/likelion/likelion_spring/repository/JdbcMemberRepository.java
@@ -0,0 +1,56 @@
+package likelion.likelion_spring.repository;
+
+import likelion.likelion_spring.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository {
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, member.getName());
+            pstmt.executeUpdate();
+            rs= pstmt.getGeneratedKeys();
+        }
+
+        if (rs.next()) {
+            member.setId(rs.getLong(1));
+        }
+        else {
+            throw new SQLException("id 조회 실패");
+        }
+        return member;
+    }
+}catch (Exception e) {
+        throw new IllegalStateException(e);
+finally {
+close(conn, pstmt, rs);
+        }
+
+
+
+
+
+
+                }
+
+
