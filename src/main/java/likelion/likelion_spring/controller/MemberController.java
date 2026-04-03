e40b9ccf3e11087aab67f4068ac784f8f789c0db
hee0543 <chaehee0543@gmail.com>
2026-04-03 22:24:43 +0900

    [섹션6] 19. 회원 웹 기능-조회

--- src/main/java/likelion/likelion_spring/controller/MemberController.java
+++ src/main/java/likelion/likelion_spring/controller/MemberController.java
@@ -1,11 +1,14 @@
 package likelion.likelion_spring.controller;
 
+import org.springframework.ui.Model;
 import likelion.likelion_spring.domain.Member;
 import likelion.likelion_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,6 +33,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
