bf9e595e9dfe3e9cf52c67f5befb442f5044fa82
hs-2171395-seungyoonkim <rlatmddbs01@naver.com>
2026-05-06 20:48:22 +0900

    feat: UserService 작성

--- Seungyoon/week6/demo/src/main/java/com/likelion/demo/domain/user/service/UserService.java
+++ Seungyoon/week6/demo/src/main/java/com/likelion/demo/domain/user/service/UserService.java
@@ -1,4 +1,30 @@
 package com.likelion.demo.domain.user.service;
 
+import com.likelion.demo.domain.user.entity.User;
+import com.likelion.demo.domain.user.exception.UserAlreadyExistExeption;
+import com.likelion.demo.domain.user.repository.UserRepository;
+import com.likelion.demo.domain.user.web.dto.UserSaveReq;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void save(UserSaveReq req){
+        userRepository.findByUsername(req.getUsername())
+                .ifPresent(user -> {
+                    throw new UserAlreadyExistExeption();
+                });
+
+        User user = User.builder()
+                .username(req.getUsername())
+                .gender(req.getGender())
+                .build();
+
+        userRepository.save(user);
+    }
 }
