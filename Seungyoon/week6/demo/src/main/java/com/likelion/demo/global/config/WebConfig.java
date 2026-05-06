1b85ddb9f1edc2b990670f9d9da182239e248e08
hs-2171395-seungyoonkim <rlatmddbs01@naver.com>
2026-05-06 20:48:01 +0900

    feat: WebConfig CORS 설정 작성

--- Seungyoon/week6/demo/src/main/java/com/likelion/demo/global/config/WebConfig.java
+++ Seungyoon/week6/demo/src/main/java/com/likelion/demo/global/config/WebConfig.java
@@ -1,4 +1,17 @@
 package com.likelion.demo.global.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //CORS
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true);
+    }
 }
