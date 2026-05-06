b7ddc43ca84db4eaf7de597b297781665e9fdd11
hs-2171395-seungyoonkim <rlatmddbs01@naver.com>
2026-05-06 20:48:12 +0900

    feat: UserAlreadyExistException 클래스 작성

--- Seungyoon/week6/demo/src/main/java/com/likelion/demo/domain/user/exception/UserAlreadyExistExeption.java
+++ Seungyoon/week6/demo/src/main/java/com/likelion/demo/domain/user/exception/UserAlreadyExistExeption.java
@@ -1,4 +1,9 @@
 package com.likelion.demo.domain.user.exception;
 
-public class UserAlreadyExistExeption {
+import com.likelion.demo.global.exception.BaseException;
+
+public class UserAlreadyExistExeption extends BaseException {
+    public UserAlreadyExistExeption() {
+        super(UserErrorCode.USER_ALREADY_EXIST_409);
+    }
 }
