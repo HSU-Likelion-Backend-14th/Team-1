2c127df6a3b46e1f87444da4cb1546e43b53cade
tack <shy66681@naver.com>
2026-05-07 16:46:29 +0900

    feat : UserService 및 관련 코드 추가

--- Gyeongtak/week6/demo/demo/src/main/java/com/likelion/demo/domain/user/reporsitory/UserRepository.java
+++ Gyeongtak/week6/demo/demo/src/main/java/com/likelion/demo/domain/user/reporsitory/UserRepository.java
@@ -1,9 +1,13 @@
 package com.likelion.demo.domain.user.reporsitory;
 
 import com.likelion.demo.domain.user.entitiy.User;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(@NotBlank(message = "이름은 필수입니다.") String username);
 }
