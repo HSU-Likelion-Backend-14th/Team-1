42c0d2dbd05fb40a2ed58f6e3cedaf53406e77f9
hee0543 <chaehee0543@gmail.com>
2026-05-08 06:31:18 +0900

    fix: MySQL 비밀번호와 아이디 재입력

--- Chaehui/week6/demo/src/main/java/com/likelion/demo/domain/user/entity/User.java
+++ Chaehui/week6/demo/src/main/java/com/likelion/demo/domain/user/entity/User.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 @Getter
 @AllArgsConstructor
